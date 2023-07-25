871815fd3d6d90f0224512ee037be94b1b35915d
Nariman Abdullin <nulls.narik@gmail.com>
2023-07-25 16:12:01 +0300

    test for debian

---
+++ src/commonTest/kotlin/com/saveourtool/osv4k/AbstractOsvSchemaTest.kt
@@ -0,0 +1,25 @@
+package com.saveourtool.osv4k
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+object OsvSchemaTestUtil {
+    @OptIn(ExperimentalSerializationApi::class)
+    private val prettyJson = Json {
+        prettyPrint = true
+        prettyPrintIndent = "  "
+    }
+
+    fun doEncodeDecodeAndCompare(
+        originalContent: String,
+    ) {
+        val result = assertNotNull(
+            Json.decodeFromString<RawOsvSchema>(originalContent)
+        )
+        assertEquals(originalContent, prettyJson.encodeToString(result))
+    }
+}
