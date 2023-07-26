d93fff20dcc7865719480604334356055abfa2b5
Nariman Abdullin <nulls.narik@gmail.com>
2023-07-26 14:03:18 +0300

    added a pure test on Java

---
+++ src/jvmTest/java/com/saveourtool/osv4k/OsvSchemaJacksonJavaTestUtil.java
@@ -0,0 +1,39 @@
+package com.saveourtool.osv4k;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.kotlin.KotlinModule;
+import org.intellij.lang.annotations.Language;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+class OsvSchemaJacksonJavaTestUtil {
+    private static final ObjectMapper objectMapper = new ObjectMapper()
+        .registerModule(new JavaTimeModule())
+        .registerModule(new KotlinModule.Builder().build())
+        .setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+    private static final ObjectWriter prettyWriter = objectMapper.writerWithDefaultPrettyPrinter()
+        .with(new DefaultPrettyPrinter().withoutSpacesInObjectEntries());
+
+    @SuppressWarnings("rawtypes")
+    static void doEncodeDecodeAndCompare(
+        final @Language("JSON") String originalContent
+    ) throws JsonProcessingException {
+        final OsvSchema result = objectMapper.readValue(originalContent, OsvSchema.class);
+        assertNotNull(result);
+        compareJsonContent(originalContent, prettyWriter.writeValueAsString(result));
+    }
+
+    private static void compareJsonContent(
+        final String contentExpected,
+        final String contentActual
+    ) throws JsonProcessingException {
+        assertEquals(objectMapper.readTree(contentExpected), objectMapper.readTree(contentActual));
+    }
+}
